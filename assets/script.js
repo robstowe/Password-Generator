eec2d8b0b81af374ac4421fe7220651320206740
robstowe <robstowe94@gmail.com>
2023-05-18 13:09:12 -0700

    updates to js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chars = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var numb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var userPickedCharacterSet = []
 // Write password to the #password input
 function writePassword() {
-//alert("working")
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //alert("working")
+  var passwordLength = window.prompt("enter a number between 8 and 128")
+  console.log(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength);
+    var isUpperCase = confirm("Do you want uppercase characters?")
+    console.log(isUpperCase);
+    var isNumber = confirm("Do you want numbers?")
+    console.log(isNumber);
+    var isLowerCase = confirm("Do you want lowercase characters?")
+    console.log(isLowerCase);
+    var isChars = confirm("Do you want special characters?")
+    console.log(isChars);
+    if (isUpperCase || isLowerCase || isNumber || isChars) {
+      if (isUpperCase) {
+        userPickedCharacterSet = userPickedCharacterSet.concat(upperC)
+        if (isNumber) {
+          userPickedCharacterSet = userPickedCharacterSet.concat(numb)
+          if (isLowerCase) {
+            userPickedCharacterSet = userPickedCharacterSet.concat(lowerC)
+            if (isChars) {
+              userPickedCharacterSet = userPickedCharacterSet.concat(chars)
+              console.log(userPickedCharacterSet);
+              for (var i = 0; i < passwordLength; i++)
+                console.log("hello");
+              var randomChars = userPickedCharacterSet[Math.floor(Math.random() * userPickedCharacterSet.length)]
+              console.log(randomChars);
+            }
+          } else {
+            console.log("Please pick at least 1 character set");
+            writePassword();
+          }
+        }
 
-  passwordText.value = password;
-
+      }
+    }
+  }
+  var passwordInput = {
+    passwordLength : passwordLength,
+    isUpperCase : isUpperCase,
+    isLowerCase : isLowerCase,
+    isNumber : isNumber,
+    isChars : isChars,
+  };
+  return passwordInput
+  console.log("gotit");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
+function generatePassword() {
   return "hello"
 
 }
