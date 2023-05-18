ed2ace24519c61a6e2be0d2b0d09f49b0de7d50c
robstowe <robstowe94@gmail.com>
2023-05-18 08:36:33 -0700

    downloaded source code

---
+++ assets/style.css
@@ -0,0 +1,34 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+//alert("working")
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  return "hello"
+
+}
+
+
+//ask for length of password
+  //prompt
+  //save it to a variable
+//check the length provided by the user is a number and in length between 8 and 128
+
+//confirm : 4 for uppercase, lowercase, numbers, and special characters
+  //save it to a variable
+//check if uppercase or lowercase or num or spec charac is there
+  // combine the confirmed arrays
+//create an arry for uppercase letters, lowercase letters, numbers, and special characters. 4 total arrays
+//create an array variable to have a new password with length provided by user. []
+//random item from combined array and push it to new password array and do this step up to the length of user input
