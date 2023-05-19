d1ca947fc0ba62608ee2f86aafdd3beb7c060ea3
robstowe <robstowe94@gmail.com>
2023-05-19 07:47:27 -0700

    finalized the code to work properly

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Password-Generator
+
+## Description
+Creating a random Password Generator for users to create a secure password of lengths between 8 and 128 characters
+
+## Live Link
+https://robstowe.github.io/Password-Generator/
+
--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,37 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 
 
-// Add event listener to generate button
-
-
-// why is it only using "deniuf" in only lowercase letters
 function generatePassword() {
-
+// array to house the character sets used to determine where we will be pulling the unique characters from
   var charSet = {
     useNumbers: "0123456789",
     useSpecChars: "!@#$%^&*",
     useLowerC: "abcdefghijklmnopqrstuvwxyz",
     useUpperC: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   };
+  // this determines the password length, and ParseINT confirms the users input to the closest integer
   var PWLength = parseInt(prompt("Please enter a password length between 8 and 128 characters."));
+  //checks to determine that PW length is a number, is atleast 8, or atleast 128
   if (isNaN(PWLength) || PWLength < 8 || PWLength > 128) {
     alert("Password length must be atleast 8 characters.");
   };
+
+  //user prompts to confirm asking if they would like to use the following character sets
   var useUpperC = confirm("Should the password include uppercase characters?");
   var useLowerC = confirm("Should the password include lowercase characters?");
   var useNumbers = confirm("Should the password include numbers?");
   var useSpecChars = confirm("Should the password include special characters?");
 
+  //if statements to confirm that the user selected atleast one of those character types "!(if False)"
   if (!useUpperC && !useLowerC && !useNumbers && !useSpecChars) {
     alert("Make sure to select atleast 1 character type!");
     return;
   }
+  //var character set to string together a random set of keys based on the charSets variable on line 8
   var charSet = "";
+  //if statements to connect that if a user used lowerC, etc and tying that back to the charSets variable
   if (useLowerC) {
     charSet += charSet.useLowerC;
   }
@@ -42,10 +44,14 @@ function generatePassword() {
   if (useSpecChars) {
     charSet += charSet.useSpecChars;
   }
+  // console.log(charSet);
   var password = "";
+
+  //for loop to loop the pw characters and to meet the required PWLength request
   for (var i = 0; i < PWLength; i++) {
     password += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
+  console.log(password);
   return password;
 }
 function writePassword() {
